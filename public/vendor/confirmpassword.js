27ff072eab64e922fc72414620f8145f4e3fd8e6
Adiba Nabiha <96282864+AdibaNabiha@users.noreply.github.com>
2023-02-17 16:34:25 +0800

    Add files via upload

---
+++ public/vendor/confirmpassword.js
@@ -0,0 +1,21 @@
+function Validate() {
+    var password = document.getElementById("txtPassword").value;
+    var confirmPassword = document.getElementById("txtConfirmPassword").value;
+    if (password != confirmPassword) {
+        alert("Passwords do not match.");
+        return false;
+    }
+    return true;
+}
+
+
+  function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  
